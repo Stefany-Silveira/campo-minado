76d55a5bd2122b6703234d5708a7ad53b98d73e5
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-03 09:56:37 -0300

    add @ Melhorando o Método toString

--- src/main/java/br/com/stefany/cm/modelo/Tabuleiro.java
+++ src/main/java/br/com/stefany/cm/modelo/Tabuleiro.java
@@ -82,17 +82,28 @@ public class Tabuleiro {
     public String toString() {
       StringBuilder sb = new StringBuilder();
 
-      int i = 0;
-      for (int l = 0; l < linhas; l++) {
+        sb.append("  ");
+        for (int c = 0; c < colunas; c++) {
+            sb.append(" ");
+            sb.append(c);
+            sb.append(" ");
+        }
 
+        sb.append("\n");
+
+        int i = 0;
+      for (int l = 0; l < linhas; l++) {
+          sb.append(l);
+          sb.append(" ");
           for (int c = 0; c < colunas; c++) {
               sb.append(" ");
-          sb.append(campos.get(i));
+              sb.append(campos.get(i));
               sb.append(" ");
               i++;
           }
           sb.append("\n");
       }
+
       return sb.toString();
     }
 }
